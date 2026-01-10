9d55246a38d2badcf414641e4ad603fef077f6d7
ChanaveerSB <chanaveersb@gmail.com>
2026-01-10 22:38:37 +0530

    pallindrome

---
+++ Basics/Recursion/P9_PallindromeStringCheck.java
@@ -0,0 +1,16 @@
+package DSA.Basics.Recursion;
+
+public class P9_PallindromeStringCheck {
+    public static boolean isPallindrome(String s,int i,int j){
+        if(i>=j) return true;
+        if(s.charAt(i)!=s.charAt(j)) return false;
+        return isPallindrome(s,i+1,j-1);
+    }
+    public static void main(String[] args) {
+        String s1="civic";
+        String s2="Bestest";
+
+        System.out.println(isPallindrome(s1,0,s1.length()-1));
+        System.out.println(isPallindrome(s2,0,s2.length()-1));
+    }
+}
