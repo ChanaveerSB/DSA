4948626e96ddbfd897c8d266fe51b9ad59c22394
ChanaveerSB <chanaveersb@gmail.com>
2026-01-28 19:13:35 +0530

    recursive sort

--- F2_Sorting/P1_SelectionSort.java
+++ F2_Sorting/P1_SelectionSort.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class P1_SelectionSort {
     static void selectionSort(int[] arr,int n){
         int min = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             min = i;
-            for(int j=i+1;j<n-1;j++){
+            for(int j=i+1;j<n;j++){
                 if(arr[j]<arr[min]){
                     min = j;
                 }
