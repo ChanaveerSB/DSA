ebfc9ee6a65fbc535adae130ee654a9d48ffa08f
ChanaveerSB <chanaveersb@gmail.com>
2026-02-12 02:30:40 +0530

    array correctionn

--- F3_Arrays/P10_MissingNumber.java
+++ F3_Arrays/P10_MissingNumber.java
@@ -1,7 +1,7 @@
 package F3_Arrays;
 
 import java.util.Arrays;
-
+//array ranging from 0 to n
 public class P10_MissingNumber {
     public static int missingNumber(int[] nums) {
     int n = nums.length;
@@ -17,7 +17,7 @@ public class P10_MissingNumber {
     }
 
     public static void main(String[] args) {
-        int[] nums={5,3,7,4,2,1};
+        int[] nums={5,3,7,4,2,1,0};
         System.out.println(Arrays.toString(nums));
         System.out.println(missingNumber(nums));
     }
