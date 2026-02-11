3e42f3f38d0c426cedb3a13716e38c6005a979e9
ChanaveerSB <chanaveersb@gmail.com>
2026-02-12 02:39:10 +0530

    missing number

--- F3_Arrays/P10_MissingNumber.java
+++ F3_Arrays/P10_MissingNumber.java
@@ -3,6 +3,7 @@ package F3_Arrays;
 import java.util.Arrays;
 //array ranging from 0 to n
 public class P10_MissingNumber {
+    //optimal method
     public static int missingNumber(int[] nums) {
     int n = nums.length;
     int expected = n * (n + 1) / 2;
@@ -16,10 +17,36 @@ public class P10_MissingNumber {
 
     }
 
+    //normal method
+    public static int missingNum(int[] arr) {
+        int n = arr.length + 1;
+
+        // Iterate from 0 to n and check
+        // if the current number is present
+        for (int i = 0; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // If the current number is not present
+            if (!found)
+                return i;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] nums={5,3,7,4,2,1,0};
+        // int[] nums={5,3,7,4,2,1,0,6};
+
         System.out.println(Arrays.toString(nums));
-        System.out.println(missingNumber(nums));
+        System.out.println(missingNum(nums));   //normal
+        System.out.println(missingNumber(nums));    //optimal
+
     }
 }
 
