08e84e9fd2d2ac27f7e7a4c9700e915db965ca68
ChanaveerSB <chanaveersb@gmail.com>
2026-02-14 23:01:17 +0530

    array

--- F3_Arrays/P12_MaxLenArrSumK.java
+++ F3_Arrays/P12_MaxLenArrSumK.java
@@ -2,6 +2,41 @@ package F3_Arrays;
 
 public class P12_MaxLenArrSumK {
     //optimal method
+    public static int longestSubarrayOptimal(int[] nums, int k) {
+        int n = nums.length;
+
+        // To store the maximum length of the subarray
+        int maxLen = 0;
+
+        // Pointers for sliding window
+        int left = 0, right = 0;
+
+        // Sum of the current window
+        int sum = nums[0];
+
+        // Traverse through the array
+        while (right < n) {
+
+            // Shrink the window if sum exceeds k
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            // Update max length if sum equals k
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Expand the window to the right
+            right++;
+            if (right < n) {
+                sum += nums[right];
+            }
+        }
+
+        return maxLen;
+    }
 
     //normal method
     public static int longestSubarray(int[] nums, int k) {
@@ -31,6 +66,7 @@ public class P12_MaxLenArrSumK {
         int[] arr={10,5,2,7,1,9};
         int k=15;
 
-        System.out.println(longestSubarray(arr, k));
+        // System.out.println(longestSubarray(arr, k));
+        System.out.println(longestSubarrayOptimal(arr, k));
     }
 }
