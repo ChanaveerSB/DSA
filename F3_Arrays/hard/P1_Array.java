46b282d534315a7d1cc772be730dfcf4101514a5
ChanaveerSB <chanaveersb@gmail.com>
2026-02-20 23:43:49 +0530

    array

--- F3_Arrays/hard/P1_Array.java
+++
@@ -1,7 +0,0 @@
-package F3_Arrays.hard;
-
-public class P1_Array {
-    public static void main(String[] args) {
-        System.out.println("heelo hard arrays");
-    }
-}
--- F3_Arrays/medium/P1_Array.java
+++
@@ -1,7 +0,0 @@
-package F3_Arrays.medium;
-
-public class P1_Array {
-    public static void main(String[] args) {
-        System.out.println("New medium program");
-    }
-}
