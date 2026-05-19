aeda383c0339865d40c006ff56e30099d00956e0
ChanaveerSB <chanaveersb@gmail.com>
2026-05-19 23:24:13 +0530

    P12 updated for all cases

--- DSA/F3_Arrays/Easy/Easy/P12_MaxLenArrSumK.java
+++ DSA/F3_Arrays/Easy/Easy/P12_MaxLenArrSumK.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class P12_MaxLenArrSumK {
-    //optimal method
+    // optimal method
     public static int longestSubarrayOptimal(int[] nums, int k) {
         int n = nums.length;
 
@@ -36,77 +36,73 @@ public class P12_MaxLenArrSumK {
             if (right < n) {
                 sum += nums[right];
             }
-            
+
         }
 
         return maxLen;
     }
 
-    //Better method
-    public static int maxLen(int[] A, int n) {
-        // map prefix sum -> first index seen
-        Map<Integer, Integer> mpp = new HashMap<>();
-        // best length so far
-        int maxi = 0;
-        // running prefix sum
-        int sum = 0;
+    // Better method
+    public static int maxLen(int[] A, int K) {
+        int n=A.length;
 
-        // iterate over the array
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;    //to store sum till the current index values
+        int maxi = 0;   //to store max length of arr
+
         for (int i = 0; i < n; i++) {
-            // update running sum
+
             sum += A[i];
 
-            // if sum is zero, subarray [0..i] has zero sum
-            if (sum == 0) {
-                // update best length
+            // if subarray from 0 to i itself gives K
+            if (sum == K) {
                 maxi = i + 1;
             }
-            // otherwise check if this sum was seen before
-            else {
-                // when seen, zero-sum segment between previous index + 1 and i
-                if (mpp.containsKey(sum)) {
-                    // maximize length
-                    maxi = Math.max(maxi, i - mpp.get(sum));
-                }
-                // first time seeing this sum
-                else {
-                    // record index
-                    mpp.put(sum, i);
-                }
+
+            // check if (sum-K) existed before
+            if (map.containsKey(sum - K)) {
+
+                maxi = Math.max(maxi,
+                        i - map.get(sum - K));
+            }
+
+            // store first occurrence only
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }
         }
 
-        // return best length
         return maxi;
     }
 
-    //Brute method
+    // Brute method
     public static int longestSubarray(int[] nums, int k) {
-       int n=nums.length;
+        int n = nums.length;
 
-       int sum;
-       int max=0;   //max lenght of array whose sum is k
-       
-       //start index subarray always between 0 to n
-       for(int i=0;i<n;i++){
-        //end index of subarray always between **i** to n
-        for(int j=i;j<n;j++){
-            sum=0;
-            for(int a=i;a<=j;a++){
-                sum = sum + nums[a];
-            }
-            if(sum == k){
-                max = Math.max(max , j - i + 1);
+        int sum;
+        int max = 0; // max lenght of array whose sum is k
+
+        // start index subarray always between 0 to n
+        for (int i = 0; i < n; i++) {
+            // end index of subarray always between **i** to n
+            for (int j = i; j < n; j++) {
+                sum = 0;
+                for (int a = i; a <= j; a++) {
+                    sum = sum + nums[a];
+                }
+                if (sum == k) {
+                    max = Math.max(max, j - i + 1);
+                }
             }
         }
-       }
 
-       return max;
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] arr={10,5,2,7,1,9};
-        int k=15;
+        int[] arr = { 10, 5, 2, 7, 1, 9 };
+        int k = 15;
 
         // System.out.println(longestSubarray(arr, k));
         // System.out.println(maxLen(arr, k));
