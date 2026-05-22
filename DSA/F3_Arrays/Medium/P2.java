e6a4dcfd664a756f41ac24423a39e94712a9beee
ChanaveerSB <chanaveersb@gmail.com>
2026-05-22 23:24:19 +0530

    P2 Medium Level

---
+++ DSA/F3_Arrays/Medium/P2.java
@@ -0,0 +1,43 @@
+package F3_Arrays.Medium;
+
+public class P2 {
+    // Function to sort the array containing only 0s, 1s and 2s
+    public static void sortZeroOneTwo(int[] nums) {
+        // Initialize count variables for 0s, 1s, and 2s
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        // Count the frequency of 0s, 1s, and 2s
+        for(int num : nums) {
+            if(num == 0) count0++;
+            else if(num == 1) count1++;
+            else count2++;
+        }
+
+        // Overwrite the array with sorted values
+        int index = 0;
+
+        // Fill with 0s
+        while(count0-- > 0) {
+            nums[index++] = 0;
+        }
+
+        // Fill with 1s
+        while(count1-- > 0) {
+            nums[index++] = 1;
+        }
+
+        // Fill with 2s
+        while(count2-- > 0) {
+            nums[index++] = 2;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 2, 1, 0};
+
+        P2.sortZeroOneTwo(nums);
+
+        for(int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
