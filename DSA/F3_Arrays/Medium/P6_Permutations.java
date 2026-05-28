6e796ef7afd71970c9235e0bcc3e8be9720b02cf
ChanaveerSB <chanaveersb@gmail.com>
2026-05-29 00:34:30 +0530

    Permutations

--- DSA/F3_Arrays/Medium/P6_Permutations.java
+++ DSA/F3_Arrays/Medium/P6_Permutations.java
@@ -5,6 +5,37 @@ import java.util.List;
 
 public class P6_Permutations {
 
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        List<Integer> current = new ArrayList<>();
+
+        dfs(nums, visited, current, result);
+        return result;
+    }
+
+    private static void dfs(int[] nums, boolean[] visited,
+                     List<Integer> current,
+                     List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            current.add(nums[i]);
+
+            dfs(nums, visited, current, result);
+
+            current.remove(current.size() - 1); // backtrack
+            visited[i] = false;
+        }
+    }
+
     private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
@@ -27,7 +58,7 @@ public class P6_Permutations {
         }
     }
     
-    public static List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute1(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         backtrack(nums, 0, result);
         return result;
@@ -37,8 +68,10 @@ public class P6_Permutations {
         int[] arr={1,2,3};
 
         List<List<Integer>> ans=permute(arr);
+        List<List<Integer>> ans1=permute1(arr);
 
         System.out.println(ans);
+        System.out.println(ans1);
         
     }
 }
